de4092cf133b10c213d39fd2fd8681a5927b8e6c
Libra-yzl <109570534+Libra-yzl@users.noreply.github.com>
2022-08-02 22:48:10 +0800

    提交代码

---
+++ test_8_2/test.c
@@ -0,0 +1,253 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5 };
+//    short* p = (short*)arr;
+//    int i = 0;
+//    for (i = 0; i < 4; i++)
+//    {
+//        *(p + i) = 0;
+//    }
+//
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	unsigned long pulArray[] = { 6,7,8,9,10 };
+//	unsigned long* pulPtr;
+//	pulPtr = pulArray;
+//	*(pulPtr + 3) += 3;
+//	printf("% d, %d\n" ,*pulPtr, *(pulPtr + 3));
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    int a = 0x11223344;
+//    char* pc = (char*)&a;
+//    *pc = 0;
+//    printf("%x\n", a);
+//    return 0;
+//}
+
+////写一个函数打印arr数组的内容，不使用数组下标，使用指针。
+////arr是一个整形一维数组。
+//#include <stdio.h>
+//
+//void print(int* arr, int sz)
+//{
+//	int i = 0;
+//	/*for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(arr + i));
+//	}*/
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *arr);
+//		arr++;
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr,sz);
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <string.h>
+//
+//void reverse(char* arr)
+//{
+//	int len = strlen(arr);
+//	char* left = arr;
+//	char* right = arr + len - 1;
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[20];
+//	scanf("%[^\n]", arr);
+//	reverse(arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+//打印菱形
+//1 3 5 7 9 11 13 11 9 7 5 3 1 *
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 7; i++)
+//	{
+//		for (j = 1; j <= 7 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 1; j <= 2 * i - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 1; i < 7; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 1; j <= 2 * 7 - 2 * i - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//求出0～100000之间的所有“水仙花数”并输出。
+//“水仙花数”是指一个n位数，其各位数字的n次方之和确好等于该数本身，
+//如:153＝1 ^ 3＋5 ^ 3＋3 ^ 3，则153是一个“水仙花数”。
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100000; i++)
+//	{
+//		//1.计算有几位数字
+//		int n = 1;  //一个数字至少有一位数
+//		int tmp = i;       //如果使用i，每次i的值将会变，可能会造成死循环
+//	                       //因此创建临时变量
+//		while (tmp /= 10)
+//		{
+//			n++;
+//		}
+//		//2.计算各位数字的n次方之和sum
+//		int sum = 0;
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += (int)pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//3.比较sum和i
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d %d", &a, &n);
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	int i = 0;
+//	//打印上半部分
+//	for (i = 0; i < line; i++)
+//	{
+//		int j = 0;
+//		//打印空格
+//		for (j = 0; j < line - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		//打印完一行换行
+//		printf("\n");
+//	}
+//	//打印下半部分
+//	for (i = 0; i < line - 1; i++)
+//	{
+//		int j = 0;
+//			//打印空格
+//		for (j = 0; j < i + 1; j++)
+//		{
+//			printf(" ");
+//		}
+//			//打印*
+//		for (j = 0; j < 2 * (line -1 - i) - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 99; i <= 999; i++)
+//	{
+//		//计算各位数
+//		int tmp = i;
+//		int n = 1;
+//		while (tmp /= 10)
+//		{
+//			n++;
+//		}
+//		//计算个位数的n次方之和
+//		tmp = i;
+//		int sum = 0;
+//		while (tmp)
+//		{
+//			sum += (int)pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//比较i和sum
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//		
+//	}
+//	return 0;
+//}
