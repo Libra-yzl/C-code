9a19361fa77a1aec15c8d15895335478223a64c3
Libra-yzl <109570534+Libra-yzl@users.noreply.github.com>
2023-06-12 22:48:18 +0800

    提交代码

---
+++ code-6_12/test.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Á´±íOJ(leetcode)" << endl ;
+	cout << "https://leetcode.cn/problems/aMhZSa/" << endl;
+	cout << "https://leetcode.cn/problems/UHnkqh/" << endl;
+	cout << "https://leetcode.cn/problems/fu-za-lian-biao-de-fu-zhi-lcof/" << endl;
+	return 0;
+}
