6b170bf988a17dd0334839730c9f6470f5bdbef7
Libra-yzl <109570534+Libra-yzl@users.noreply.github.com>
2023-06-17 22:46:26 +0800

    提交代码

---
+++ test-6_17/B.c
@@ -0,0 +1,250 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include "B.h"
+
+// 通过前序遍历的数组"ABD##E#H##CF##G##"构建二叉树
+BTNode* BinaryTreeCreate(BTDataType* a, int n, int* pi)
+{
+	if (a[*pi] == '#')
+	{
+		(*pi)++;
+		return NULL;
+	}
+
+	BTNode* newnode = (BTNode*)malloc(sizeof(BTNode));
+	if (!newnode)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+	newnode->data = a[(*pi)++];
+	newnode->left = BinaryTreeCreate(a, n, pi);
+	newnode->right = BinaryTreeCreate(a, n, pi);
+
+	return newnode;
+}
+
+// 二叉树销毁
+void BinaryTreeDestory(BTNode* root)
+{
+	if (root == NULL)
+		return;
+
+	BinaryTreeDestory(root->left);
+	BinaryTreeDestory(root->right);
+	free(root);
+}
+
+// 二叉树节点个数
+int BinaryTreeSize(BTNode* root)
+{
+	return root == NULL ? 0 : BinaryTreeSize(root->left) + BinaryTreeSize(root->right) + 1;
+}
+
+// 二叉树叶子节点个数
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+	if (root->left == NULL && root->right == NULL)
+		return 1;
+	return BinaryTreeLeafSize(root->left) + BinaryTreeLeafSize(root->right);
+}
+
+// 二叉树第k层节点个数
+int BinaryTreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+		return 0;
+	if (k == 1)
+		return 1;
+	return BinaryTreeLevelKSize(root->left, k - 1) + BinaryTreeLevelKSize(root->right, k - 1);
+}
+
+// 二叉树查找值为x的节点
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+		return NULL;
+	if (root->data == x)
+		return root;
+	BTNode* ret1 = BinaryTreeFind(root->left, x);
+	if (ret1)
+		return ret1;
+	BTNode* ret2 = BinaryTreeFind(root->right, x);
+	if (ret2)
+		return ret2;
+
+	return NULL;
+}
+
+// 二叉树前序遍历 
+void BinaryTreePrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+
+	printf("%c ", root->data);
+	BinaryTreePrevOrder(root->left);
+	BinaryTreePrevOrder(root->right);
+}
+
+// 二叉树中序遍历
+void BinaryTreeInOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+
+	BinaryTreeInOrder(root->left);
+	printf("%c ", root->data);
+	BinaryTreeInOrder(root->right);
+}
+
+// 二叉树后序遍历
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+
+	BinaryTreePostOrder(root->left);
+	BinaryTreePostOrder(root->right);
+	printf("%c ", root->data);
+}
+
+// 层序遍历
+void BinaryTreeLevelOrder(BTNode* root)
+{
+	Queue q;
+	QueueInit(&q);
+	if (root)
+		QueuePush(&q, root);
+
+	while (!QueueEmpty(&q))
+	{
+		BTNode* front = QueueFront(&q);
+		printf("%c ", front->data);
+		QueuePop(&q);
+
+		if (front->left)
+		{
+			QueuePush(&q, front->left);
+		}
+		if (front->right)
+		{
+			QueuePush(&q, front->right);
+		}
+	}
+	QueueDestroy(&q);
+}
+
+// 判断二叉树是否是完全二叉树
+int BinaryTreeComplete(BTNode* root);
+
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+
+	pq->head = pq->tail = NULL;
+	pq->size = 0;
+}
+void QueueDestroy(Queue* pq)
+{
+	assert(pq);
+	if (pq->head == NULL)
+		return;
+
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		QNode* node = cur->next;
+		free(cur);
+		cur = node;
+	}
+	pq->head = pq->tail = NULL;
+	pq->size = 0;
+
+}
+
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+	if (!newnode)
+		return;
+	newnode->val = x;
+	newnode->next = NULL;
+
+	if (pq->tail == NULL)
+	{
+		pq->head = pq->tail = newnode;
+	}
+	else
+	{
+		pq->tail->next = newnode;
+		pq->tail = newnode;
+	}
+	pq->size++;
+}
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(pq->head);
+
+	if (pq->head->next == NULL)
+	{
+		free(pq->head);
+		pq->head = pq->tail = NULL;
+	}
+	else
+	{
+		QNode* node = pq->head->next;
+		free(pq->head);
+		pq->head = node;
+	}
+	pq->size--;
+}
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(pq->head);
+
+	return pq->head->val;
+}
+
+int QueueSize(Queue* pq)
+{
+	assert(pq);
+
+	return pq->size;
+}
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+
+	return pq->head == pq->tail && pq->head == NULL;
+}
+
+//void Print(Queue* pq)
+//{
+//	assert(pq);
+//
+//	QNode* cur = pq->head;
+//	while (cur)
+//	{
+//		printf("%d ", cur->val);
+//		cur = cur->next;
+//	}
+//	printf("\n");
+//}
